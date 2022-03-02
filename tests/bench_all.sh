5c57dd27effee12d4c246fce51f8cfa617772ad7
Robert Primas <robert.primas@iaik.tugraz.at>
2022-03-02 20:46:47 +0100

    bench script

---
+++ tests/bench_all.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+cd ..
+
+echo "#################"
+echo "#      AEAD     #"
+echo "#################"
+declare -a arr=(
+"crypto_aead/isapa128av20/opt_64"
+"crypto_aead/isapa128av20/opt_64_compact"
+"crypto_aead/isapa128av20/opt_64_stp"
+"crypto_aead/isapa128av20/bi_32"
+#"crypto_aead/isapa128av20/bi_32_armv6"			# works only on arm devices
+"crypto_aead/isapa128av20/bi_32_compact"
+"crypto_aead/isapa128av20/bi_32_stp"
+"crypto_aead/isapa128av20/ref"
+)
+for i in "${arr[@]}"
+do
+	echo "$i"
+	gcc -march=native -O3 -DNDEBUG -I$i $i/*.c -DCRYPTO_AEAD -Itests tests/getcycles.c -o getcycles
+	res=$(./getcycles | tail -n1)
+	echo "  64:"$(echo $res | rev | cut -d "|" -f 6 | rev)
+	echo "1536:"$(echo $res | rev | cut -d "|" -f 4 | rev)
+	echo "long:"$(echo $res | rev | cut -d "|" -f 2 | rev)
+done
+
+echo "########################"
+echo "#      AEAD + HASH     #"
+echo "########################"
+declare -a arr=(
+"crypto_aead_hash/isapa128av20/opt_64"
+"crypto_aead_hash/isapa128av20/opt_64_compact"
+"crypto_aead_hash/isapa128av20/opt_64_stp"
+"crypto_aead_hash/isapa128av20/bi_32"
+#"crypto_aead_hash/isapa128av20/bi_32_armv6"	# works only on arm devices
+"crypto_aead_hash/isapa128av20/bi_32_compact"
+"crypto_aead_hash/isapa128av20/bi_32_stp"
+)
+for i in "${arr[@]}"
+do
+	echo "$i"
+	gcc -march=native -O3 -DNDEBUG -I$i $i/*.c -DCRYPTO_AEAD -Itests tests/getcycles.c -o getcycles
+	res=$(./getcycles | tail -n1)
+	echo "  64:"$(echo $res | rev | cut -d "|" -f 6 | rev)
+	echo "1536:"$(echo $res | rev | cut -d "|" -f 4 | rev)
+	echo "long:"$(echo $res | rev | cut -d "|" -f 2 | rev)
+	gcc -march=native -O3 -DNDEBUG -I$i $i/*.c -DCRYPTO_HASH -Itests tests/getcycles.c -o getcycles
+	res=$(./getcycles | tail -n1)
+	echo "  64:"$(echo $res | rev | cut -d "|" -f 6 | rev)
+	echo "1536:"$(echo $res | rev | cut -d "|" -f 4 | rev)
+	echo "long:"$(echo $res | rev | cut -d "|" -f 2 | rev)
+done
